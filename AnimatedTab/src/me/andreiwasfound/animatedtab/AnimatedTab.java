a2e31da6155e4916856fca0770248ba86a2874c1
AndreiWasFound <54415861+AndreiWasFound@users.noreply.github.com>
2020-07-10 17:56:25 +0300

    Code 1.0 NMS

--- AnimatedTab/src/me/andreiwasfound/animatedtab/AnimatedTab.java
+++ AnimatedTab/src/me/andreiwasfound/animatedtab/AnimatedTab.java
@@ -3,13 +3,24 @@ package me.andreiwasfound.animatedtab;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.andreiwasfound.animatedtab.versions.TabManager;
+import me.andreiwasfound.animatedtab.versions.TabManager_1_12_R1;
+import me.andreiwasfound.animatedtab.versions.TabManager_1_15_R1;
+import me.andreiwasfound.animatedtab.versions.TabManager_1_16_R1;
+
 public class AnimatedTab extends JavaPlugin {
 	
 	public TabManager tab;
+	public String sversion;
 	
 	@Override
 	public void onEnable() {
-		this.tab = new TabManager(this);
+		
+		if (!setupManager()) {
+			getLogger().severe("Failed to setup AnimatedTab! Running non-compatible version!");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
 		
 		tab.addHeader("&c&lCodedRed\n&9Join the help discord");
 		tab.addHeader("&4&lCodedRed\n&9Join the help discord");
@@ -22,6 +33,28 @@ public class AnimatedTab extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
+		
+	}
+	
+	private boolean setupManager() {
+		
+		sversion = "N/A";
+		
+		try {
+			sversion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+		
+		if (sversion.equals("v1_12_R1"))
+			tab = new TabManager_1_12_R1(this);
+		else if (sversion.equals("v1_15_R1"))
+			tab = new TabManager_1_15_R1(this);
+		else if (sversion.equals("v1_16_R1"))
+			tab = new TabManager_1_16_R1(this);
+		
+		return tab != null;
+				
 		
 	}
 
---
+++ AnimatedTab/src/me/andreiwasfound/animatedtab/versions/TabManager.java
@@ -0,0 +1,10 @@
+package me.andreiwasfound.animatedtab.versions;
+
+public interface TabManager {
+	
+	public void showTab();
+	public void addHeader(String header);
+	public void addFooter(String footer);
+
+
+}
